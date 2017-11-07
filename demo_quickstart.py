9b1d1894885824f4fa905ccadb1b3a18f40bba60
Abitha Ramachandran <abitharamachandran2018@u.northwestern.edu>
2017-11-06 20:54:54 -0600

    sends alerts

--- demo_quickstart.py
+++ demo_quickstart.py
@@ -3,6 +3,7 @@ import httplib2
 import os
 import base64
 import email
+from email.mime.text import MIMEText
 
 from apiclient import discovery, errors
 from oauth2client import client
@@ -10,6 +11,7 @@ from oauth2client import tools
 from oauth2client.file import Storage
 from firebase import firebase
 import time
+import re
 
 try:
     import argparse
@@ -18,7 +20,6 @@ except ImportError:
     flags = None
 
 class gmailQuerier:
-
     def __init__(self):
         # If modifying these scopes, delete your previously saved credentials
         # at ~/.credentials/gmail-python-quickstart.json
@@ -29,12 +30,10 @@ class gmailQuerier:
         self.firebase = firebase.FirebaseApplication('https://fir-demo-184316.firebaseio.com/', None)
 
         # Adding a log file to save our print statements
-        self.log = open('log_GmailAPI_2017_11_02.txt', 'a')
-        self.log.write("We have started querying the GmailAPI!" + "\n")
 
-    def post_new_texts(self, name, time, snippet):
+    def post_new_texts(self, name, time, newTime, snippet):
 
-        self.firebase.post('/users/' + name + '/', {'time': time, 'message': snippet})
+        self.firebase.post('/users/' + name + '/', {'time': time, 'newTime': newTime, 'message': snippet})
         newresult = self.firebase.get('/users/', name)
         print('We have added this entry: %s' % newresult)
 
@@ -89,36 +88,32 @@ class gmailQuerier:
         of the user's Gmail account.
         """
         credentials = self.get_credentials()
+        localtime = time.asctime( time.localtime(time.time()) )
+        print ('Local current time: %s' % localtime)
         http = credentials.authorize(httplib2.Http())
         service = discovery.build('gmail', 'v1', http=http)
 
+        
+
         while True:
-            time.sleep(2) #Run the function every 2 seconds to avoid API rate limit error
             try:
                 messageids = self.list_messages_matching_query(service, 'me', '')
                 if not messageids:
-                    print ("Main: There are no ids, but the script is still running")
-                    self.log.write("Main: There are no ids, but the script is still running" + "\n")
-                    self.log.flush()
+                    print ("There are no ids, but the script is still running")
                 for ids in messageids:
-                    print ("Main: before get_message")
                     self.get_message(service, 'me', ids['id'])
-
+                
             except KeyboardInterrupt: 
-                print ("Main: Keyboard interrupt; stopping script now")
-                self.log.write("Main: Keyboard interrupt; stopping script now" + "\n")
-                self.log.flush()
+                print ("Keyboard interrupt; stopping script now")
                 break
 
             except errors.HttpError, error:
-                print('Main: An error occurred: %s' % error)
-                self.log.write("Main: An error occurred: {0}".format(error) + "\n")
-                self.log.flush()
+                print('An error occurred: %s' % error)
 
             except Exception as err:
-                print('Main: An error occurred or there are no new messages: %s' % err)
-                self.log.write("Main: An error occurred or there are no new messages: {0}".format(err) + "\n")
-                self.log.flush()
+                print('An error occurred or there are no new messages: %s' % err)
+
+            self.checkTime(service)
             
 
     def list_messages_matching_query(self, service, user_id, query=''):
@@ -136,19 +131,8 @@ class gmailQuerier:
             appropriate ID to get the details of a Message.
         """
         try:
-            # Adding extra print statements to debug API rate limit error 429 (11/2/2017)
-            print("list_messages_matching_query: list method called? NOT YET")
-            self.log.write("list_messages_matching_query: list method called? NOT YET" + "\n")
-            self.log.flush()
-
             response = service.users().messages().list(userId=user_id,
                                                        q=query).execute()
-
-            # Adding extra print statements to debug API rate limit error 429 (11/2/2017)
-            print("list_messages_matching_query: list method called? YES!")
-            self.log.write("list_messages_matching_query: list method called? YES!" + "\n")
-            self.log.flush()
-
             messages = []
             if 'messages' in response:
               messages.extend(response['messages'])
@@ -161,9 +145,40 @@ class gmailQuerier:
 
             return messages
         except errors.HttpError, error:
-            print('list_messages_matching_query: An error occurred: %s' % error)
-            self.log.write("list_messages_matching_query: An error occurred: {0}".format(error) + "\n")
-            self.log.flush()
+            print('An error occurred within list_messages_matching_query: %s' % error)
+
+    def checkTime(self, service):
+
+        usersdb = self.firebase.get('/users', None)
+        for user in usersdb:
+            all_texts = usersdb.get(user, None)
+            for text in all_texts:
+                current_text = all_texts.get(text, None)
+                snippet = current_text.get('message')
+                newTime = current_text.get('newTime')
+                localtime = time.asctime( time.localtime(time.time()) )
+                if (newTime[:16] == localtime[:16]):
+                    print(self.sentMessages)
+                    print(snippet)
+                    if(not snippet in self.sentMessages):
+                        print ("Before create message")
+                        userTemp = str(user) + "@mms.att.net"
+                        print("This is who we're sending the alert to: " + userTemp)
+                        alert = self.create_message("holdthatthoughtapp@gmail.com", userTemp, "Don't forget about this", snippet)
+                        self.send_message(service, 'me', alert)
+                        print ("We have sent the alert!")
+                        url = 'users' + '/' + user + '/' + text
+                        self.sentMessages.append(str(snippet))
+                    else:
+                        print ("We've already seen this message")
+                    
+            '''
+            for text in users.get().each():
+                snippet = text.get('/message')
+                newTime = text.get('/newTime')
+                print('This is the snippet ' + snippet)
+                print('This is the newTime ' + newTime)
+            '''
 
     def delete_message(self, service, user_id, msg_id):
         """Delete a Message.
@@ -177,13 +192,8 @@ class gmailQuerier:
         try:
             service.users().messages().trash(userId=user_id, id=msg_id).execute()
             print('Message with id: %s deleted successfully.' % msg_id)
-            self.log.write("delete_message: Message with id {0} deleted successfully".format(msg_id) + "\n")
-            self.log.flush()
-
         except errors.HttpError, error:
-            print('delete_message: An error occurred: %s' % error)
-            self.log.write("delete_message: An error occurred: {0}".format(error) + "\n")
-            self.log.flush()
+            print('An error occurred: %s' % error)
 
     def get_message(self, service, user_id, msg_id):
         """Get a Message with given ID.
@@ -200,7 +210,6 @@ class gmailQuerier:
 
         try:
             message = service.users().messages().get(userId=user_id, id=msg_id).execute()
-            print("we got a message {0}".format(message['snippet']))
             payload = message['payload']
             headers = payload['headers']
             sender = headers[18]
@@ -209,16 +218,78 @@ class gmailQuerier:
             time = sent['value']
             print('Sender: %s' % name)
             print('Time: %s' % time)
+
             print('Message snippet: %s' % message['snippet'])
-            self.log.write("get_message: Sender: {0}, Time: {1}, Message: {2}".format(name, time, message['snippet']) + "\n")
-            self.log.flush()
-            self.post_new_texts(name[:10], time, message['snippet'])
+            newTime = self.format_time(time)
+            self.post_new_texts(name[:10], time, newTime, message['snippet'])
             self.delete_message(service, 'me', msg_id)
 
         except errors.HttpError, error:
-            print('get_message: An error occurred: %s' % error)
-            self.log.write("get_message: An error occurred: {0}".format(error) + "\n")
-            self.log.flush()
+            print('An error occurred: %s' % error)
+
+    def format_time(self, time):
+        tempList = (re.split(' ', time))
+        temptime = tempList[4]
+        temphour = int(temptime[0:2])
+        if(tempList[6] == '(CDT)' or tempList[6] == '(CST)'):
+            temphour = int(temptime[0:2]) + 1 #adding one hour for alert
+        elif(tempList[6] == '(PDT)' or tempList[6] == '(PST)'):
+            temphour = int(temptime[0:2]) + 3 #adding three hours for +2 timezone and +1 alert
+        elif(tempList[6] == '(EDT)' or tempList[6] == '(EST)'):
+            temphour = int(temptime[0:2]) #-1 for timezone +1 for alert
+        elif(tempList[6] == '(MDT)' or tempList[6] == '(MST)'):
+            temphour = int(temptime[0:2]) + 2 #+1 for timezone +1 for alert
+        if(temphour < 10): 
+            temphour = "0" + str(temphour)
+            print ("This is temphour {0}".format(temphour))
+            '''tempmin = int(temptime[3:5]) + 1;
+            if(tempmin < 10):
+                tempmin = "0" + str(tempmin)
+            print ("This is tempmin {0}".format(tempmin))
+            temptime = str(temphour) + ":" + str(tempmin) + temptime[5:]'''
+        temptime = str(temphour) + temptime[2:]
+        tempList[4] = temptime
+        print ("This is tempList: %s" % tempList)
+        timeList = [tempList[0][0:3], tempList[2], tempList[1], tempList[4], tempList[3]]
+        return timeList[0] + " " + timeList[1] + "  " + timeList[2] + " " + timeList[3] + " " + timeList[4]
+
+    def create_message(self, sender, to, subject, message_text):
+      """Create a message for an email.
+
+      Args:
+        sender: Email address of the sender.
+        to: Email address of the receiver.
+        subject: The subject of the email message.
+        message_text: The text of the email message.
+
+      Returns:
+        An object containing a base64url encoded email object.
+      """
+      message = MIMEText(message_text)
+      message['to'] = to
+      message['from'] = sender
+      message['subject'] = subject
+      return {'raw': base64.urlsafe_b64encode(message.as_string())}
+
+    def send_message(self, service, user_id, message):
+      """Send an email message.
+
+      Args:
+        service: Authorized Gmail API service instance.
+        user_id: User's email address. The special value "me"
+        can be used to indicate the authenticated user.
+        message: Message to be sent.
+
+      Returns:
+        Sent Message.
+      """
+      try:
+        message = (service.users().messages().send(userId=user_id, body=message)
+                   .execute())
+        print ('Message Id: %s' % message['id'])
+        return message
+      except errors.HttpError, error:
+        print ('An error occurred: %s' % error)
 
 myObject = gmailQuerier()
 myObject.main()
