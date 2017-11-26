98cf289bfa798f103e716016e6e390025e8920d6
Eureka Foong <eureka.foong@gmail.com>
2017-11-25 21:58:00 -0600

    changed sentMessages list to firebase reference

--- quickstart.py
+++ quickstart.py
@@ -161,22 +161,30 @@ class gmailQuerier:
                     if (self.firebase.get('/sentMessages/', user) != None): # Check there are sentMessages in the first place
                         print("-----We found some sentMessages-----")
                         sentMessagesdb = self.firebase.get('/sentMessages/', user)
+                        seen_message = False
                         for entry in sentMessagesdb:
                             current_entry = sentMessagesdb.get(entry, None)
                             if (current_entry.get('sentMessage') == snippet):
                                 print("We've already seen this message")
-                                return 
+                                seen_message = True
                             else:
-                                userTemp = str(user) + "@mms.att.net"
-                                print("This is who we're sending the alert to: " + userTemp)
-                                if(user in self.mostRecentAlerts and self.mostRecentAlerts[user] == snippet):
-                                    alert = self.create_message("holdthatthoughtapp@gmail.com", userTemp, "Another reminder", snippet)
-                                else:
-                                    alert = self.create_message("holdthatthoughtapp@gmail.com", userTemp, "Don't forget about this", snippet)
-                                self.send_message(service, 'me', alert)
-                                print ("We have sent the alert!") 
-                                url = 'users' + '/' + user + '/' + text # Do we still need this code??
-                                self.firebase.post('/sentMessages/' + user + '/', {'sentMessage': snippet}) # Add entry to sentMessages firebase
+                                continue
+                        if (seen_message == True):
+                            print("We have already seen the message and will not send an alert")
+                            return 
+                        else: # If we haven't seen the message, then send the alert!
+                            print("We have not seen the message")
+                            userTemp = str(user) + "@mms.att.net"
+                            print("This is who we're sending the alert to: " + userTemp)
+                            if(user in self.mostRecentAlerts and self.mostRecentAlerts[user] == snippet):
+                                alert = self.create_message("holdthatthoughtapp@gmail.com", userTemp, "Another reminder", snippet)
+                            else:
+                                alert = self.create_message("holdthatthoughtapp@gmail.com", userTemp, "Don't forget about this", snippet)
+                            self.send_message(service, 'me', alert)
+                            print ("We have sent the alert!") 
+                            url = 'users' + '/' + user + '/' + text # Do we still need this code??
+                            self.firebase.post('/sentMessages/' + user + '/', {'sentMessage': snippet}) # Add entry to sentMessages firebase
+
                     else: # Else, assume first time user receiving alert
                         print("-----Going into else statement -----")
                         userTemp = str(user) + "@mms.att.net"
@@ -189,6 +197,7 @@ class gmailQuerier:
                         print ("We have sent the alert!") 
                         url = 'users' + '/' + user + '/' + text # Do we still need this code??
                         self.firebase.post('/sentMessages/' + user + '/', {'sentMessage': snippet}) # Add entry to sentMessages firebase
+
 
                     
             '''
