9ad627c376569f665967e1a62821fa6ef414b8d6
Eureka Foong <eureka.foong@gmail.com>
2017-11-27 23:02:02 -0600

    minor fix to mostRecentAlert firebase object

--- quickstart.py
+++ quickstart.py
@@ -196,10 +196,16 @@ class gmailQuerier:
                             self.firebase.post('/sentMessages/' + user + '/', {'sentMessage': snippet}) # Add entry to sentMessages firebase
                             
                     else: # Else, assume first time user receiving alert
-                        print("This is the first time we are sending the user an alert!")
-                        userTemp = str(user) + "@mms.att.net"
-                        print("This is who we're sending the alert to: " + userTemp)
-                        alert = self.create_message("holdthatthoughtapp@gmail.com", userTemp, "Don't forget about this", snippet)
+                        if (self.firebase.get('/mostRecentAlert/', user) != None): # We've sent an alert before
+                            print("This is the first time we're snoozing!")
+                            alert = self.create_message("holdthatthoughtapp@gmail.com", userTemp, "Another reminder", snippet)
+                            userTemp = str(user) + "@mms.att.net"
+                            print("This is who we're sending the alert to: " + userTemp)
+                        else:
+                            print("This is the first time the user is receiving an alert!")
+                            userTemp = str(user) + "@mms.att.net"
+                            print("This is who we're sending the alert to: " + userTemp)
+                            alert = self.create_message("holdthatthoughtapp@gmail.com", userTemp, "Don't forget about this", snippet)
                         self.send_message(service, 'me', alert)
                         print ("We have sent the alert!") 
                         self.firebase.post('/sentMessages/' + user + '/', {'sentMessage': snippet}) # Add entry to sentMessages firebase
